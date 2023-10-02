49d0f9cbd9c1206c22247f2c53df7a9fd6ad446a
YoungMok Kim <indiemaru@gmail.com>
2023-10-02 14:04:06 +0900

    update feature

--- admin/src/main/java/org/beizix/admin/adapter/web/exboard/ExBoardSaveController.java
+++ admin/src/main/java/org/beizix/admin/adapter/web/exboard/ExBoardSaveController.java
@@ -2,9 +2,11 @@ package org.beizix.admin.adapter.web.exboard;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.beizix.admin.adapter.web.exboard.model.filter.ExBoardListFilterReqVO;
+import org.beizix.admin.adapter.web.exboard.model.save.ExBoardSaveAttachFormVO;
 import org.beizix.admin.adapter.web.exboard.model.save.ExBoardSaveFormVO;
 import org.beizix.core.application.domain.exboard.model.save.ExBoardSaveInput;
 import org.beizix.core.application.domain.exboard.model.save.ExBoardSaveOutput;
@@ -35,26 +37,30 @@ class ExBoardSaveController {
       BindingResult bindingResult)
       throws IOException {
 
-    if (bindingResult.hasErrors()) {
-      return "board/exBoardForm";
-    }
-
-    ExBoardSaveInput saveInput = modelMapper.map(saveReqVO, ExBoardSaveInput.class);
-
     // 수정일 경우, 대표 이미지와 비공개 첨부 정보는 전달받지 않기에 기존 저장된 정보를 조회해서 가져온다.
     if (saveReqVO.getId() != null) {
       Optional.of(exBoardViewPortIn.connect(saveReqVO.getId()))
           .ifPresent(
               viewOutput -> {
-                saveInput.setRepresentImage(viewOutput.getRepresentImage());
-                saveInput.setPrivateAttachment(viewOutput.getPrivateAttachment());
+                saveReqVO.setRepresentImage(viewOutput.getRepresentImage());
+                saveReqVO.setPrivateAttachment(viewOutput.getPrivateAttachment());
+                saveReqVO.setAttachments(
+                    viewOutput.getAttachments().stream()
+                        .map(
+                            attachment ->
+                                modelMapper.map(attachment, ExBoardSaveAttachFormVO.class))
+                        .collect(Collectors.toList()));
               });
+    }
+
+    if (bindingResult.hasErrors()) {
+      return "board/exBoardForm";
     }
 
     try {
       ExBoardSaveOutput createdItem =
           exBoardSavePortIn.connect(
-              saveInput,
+              modelMapper.map(saveReqVO, ExBoardSaveInput.class),
               saveReqVO.getRepresentImgFile(),
               saveReqVO.getMultipartPrivateAttachment(),
               saveReqVO.getMultipartAttachments());
