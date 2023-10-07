1eb493fb7e6dc8846a954e5c8275170f9df93d55
YoungMok Kim <indiemaru@gmail.com>
2023-10-08 00:18:36 +0900

    add comments to interface

--- core/src/main/java/org/beizix/core/application/port/in/exboard/ExBoardSavePortIn.java
+++ core/src/main/java/org/beizix/core/application/port/in/exboard/ExBoardSavePortIn.java
@@ -6,15 +6,22 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ExBoardSavePortIn {
-
   /**
    * 예제 게시판 저장 인터페이스
    *
-   * @param exBoard 게시물 정보
+   * @param id 아이디
+   * @param title 제목
+   * @param content 내용
+   * @param visible 공개여부
+   * @param boardStartDate 공개 시작일
+   * @param boardEndDate 공개 종료일
+   * @param repImgAlt 이미지 alt
+   * @param orderNo 정렬번호
+   * @param removeAttachmentIds 공개 첨부 파일 삭제 아이디 목록
    * @param representImgFile 대표 이미지 파일
    * @param privateAttachment 비공개 첨부 파일
    * @param publicAttachments 공개 첨부 파일 목록
-   * @return ExBoardSaveOutput
+   * @return 생성/수정된 아이템 아이디
    * @throws IOException 파일 업로드 수행 중 발생한 오류
    */
   Long connect(
