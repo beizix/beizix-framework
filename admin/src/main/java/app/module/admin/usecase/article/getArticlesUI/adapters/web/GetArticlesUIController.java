b8fa146adb9e69f6ed514ff743b04b4ff9741590
youngmok.kim <indiemaru@gmail.com>
2024-09-07 09:32:48 +0900

    update logic

--- admin/src/main/java/app/module/admin/usecase/article/getArticlesUI/adapters/web/GetArticlesUIController.java
+++ admin/src/main/java/app/module/admin/usecase/article/getArticlesUI/adapters/web/GetArticlesUIController.java
@@ -1,16 +1,8 @@
 package app.module.admin.usecase.article.getArticlesUI.adapters.web;
 
-import app.module.admin.usecase.article.getArticles.ports.GetArticlesPortIn;
-import app.module.admin.usecase.article.getArticles.ports.application.domain.GetArticles;
-import app.module.admin.usecase.article.getArticles.ports.application.domain.GetArticlesCmd;
 import app.module.admin.usecase.article.getArticlesUI.adapters.web.model.GetArticlesUIReqVO;
-import app.module.core.config.adapter.persistence.entity.Article_;
 import app.module.core.usecase.uicode.list.application.port.in.UICodeListPortIn;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,14 +14,7 @@ class GetArticlesUIController {
   private final UICodeListPortIn uiCodeListPortIn;
 
   @GetMapping(path = "/board/articles")
-  String operate(
-      @PageableDefault(sort = Article_.ORDER_NO, direction = Sort.Direction.DESC) Pageable pageable,
-      @ModelAttribute("reqVO") GetArticlesUIReqVO reqVO,
-      Model model) {
-
-    // pageable 을 통해 조회된 정렬값은 실제 요청 값과 다르다. 실제 요청값으로 변경해준다.
-    reqVO.setSort(pageable.getSort().toString().replace(": ", ","));
-    reqVO.setPage(pageable.getPageNumber());
+  String operate(@ModelAttribute("reqVO") GetArticlesUIReqVO reqVO, Model model) {
 
     model.addAttribute("pageRows", uiCodeListPortIn.connect("code.pageable.rows"));
 
--- admin/src/main/java/app/module/admin/usecase/article/getArticlesUI/adapters/web/model/GetArticlesUIReqVO.java
+++ admin/src/main/java/app/module/admin/usecase/article/getArticlesUI/adapters/web/model/GetArticlesUIReqVO.java
@@ -10,12 +10,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class GetArticlesUIReqVO {
-  // 페이지 당 아이템 수
-  private Integer size;
-  // 정렬 값
-  private String sort;
-  // 요청 페이지 넘버
-  private Integer page;
+  // 요청 페이지 넘버 - 초기값 `0`
+  private Integer page = 0;
+  // 정렬 값 - 초기값 `orderNo 내림차순`
+  private String sort = "orderNo,DESC";
+  // 페이지 당 아이템 수 - 초기값 `10`
+  private Integer size = 10;
   private String searchField;
   private String searchValue;
   private String searchOpen;
