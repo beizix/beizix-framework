485521f2bc3f056f5a7c0c9ec1b0528f8b3466c6
youngmok.kim <indiemaru@gmail.com>
2024-09-09 12:41:15 +0900

    update articles page

--- admin/src/main/java/app/module/admin/usecase/article/getArticlesUI/adapters/web/model/GetArticlesUIReqVO.java
+++ admin/src/main/java/app/module/admin/usecase/article/getArticlesUI/adapters/web/model/GetArticlesUIReqVO.java
@@ -4,19 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.thymeleaf.util.StringUtils;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @EqualsAndHashCode
 public class GetArticlesUIReqVO {
-  // 요청 페이지 넘버 - 초기값 `0`
-  private Integer page = 0;
-  // 정렬 값 - 초기값 `orderNo 내림차순`
-  private String sort = "orderNo,DESC";
-  // 페이지 당 아이템 수 - 초기값 `10`
-  private Integer size = 10;
+  private Integer page;
+  private String sort;
+  private Integer size;
   private String searchField;
   private String searchValue;
   private String searchOpen;
+
+  public Integer getPage() {
+    // 요청 페이지 넘버 - 초기값 `0`
+    return page != null ? page : 0;
+  }
+
+  public String getSort() {
+    // 정렬 값 - 초기값 `orderNo 내림차순`
+    return StringUtils.isEmpty(sort) ? "orderNo,DESC" : sort;
+  }
+
+  public Integer getSize() {
+    // 페이지 당 아이템 수 - 초기값 `10`
+    return size != null ? size : 10;
+  }
 }
