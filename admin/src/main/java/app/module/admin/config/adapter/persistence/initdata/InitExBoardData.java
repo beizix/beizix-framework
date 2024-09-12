b3110c88e7c46e0b710de3da0e4e667e8e909ad8
youngmok.kim <indiemaru@gmail.com>
2024-09-12 13:37:20 +0900

    update codes

--- admin/src/main/java/app/module/admin/config/adapter/persistence/initdata/InitExBoardData.java
+++ admin/src/main/java/app/module/admin/config/adapter/persistence/initdata/InitExBoardData.java
@@ -3,6 +3,7 @@ package app.module.admin.config.adapter.persistence.initdata;
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
+import app.module.admin.config.adapter.persistence.initdata.helper.RemoveExBoardAllRepo;
 import app.module.admin.usecase.exboard.save.ports.ExBoardSavePortOut;
 import lombok.RequiredArgsConstructor;
 import app.module.core.config.application.util.PropertyUtil;
@@ -14,10 +15,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class InitExBoardData implements ApplicationRunner {
   private final ExBoardSavePortOut exBoardSavePortOut;
+  private final RemoveExBoardAllRepo removeExBoardAllRepo;
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
     if (!PropertyUtil.isCoreDataRequired()) return;
+
+    removeExBoardAllRepo.deleteAll();
 
     IntStream.range(0, 25)
         .forEach(
