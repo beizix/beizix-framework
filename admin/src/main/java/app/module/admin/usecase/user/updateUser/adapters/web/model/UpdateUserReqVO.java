418685fdaa2b9549f724e035d3e69f78ebc013bd
youngmok.kim <indiemaru@gmail.com>
2024-09-15 11:19:07 +0900

    impl features

--- admin/src/main/java/app/module/admin/usecase/user/updateUser/adapters/web/model/UpdateUserReqVO.java
+++ admin/src/main/java/app/module/admin/usecase/user/updateUser/adapters/web/model/UpdateUserReqVO.java
@@ -1,5 +1,10 @@
 package app.module.admin.usecase.user.updateUser.adapters.web.model;
 
+import java.util.Set;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -10,5 +15,25 @@ import lombok.Setter;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class UpdateUserReqVO {
+  @NotNull(message = "{valid.common.required}")
+  private String id;
 
+  @NotBlank(message = "{valid.common.required}")
+  @Pattern(
+      regexp =
+          "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+      message = "{valid.common.email.wrong}")
+  private String email;
+
+  @NotNull(message = "{valid.common.required}")
+  private Boolean accountDisabled;
+
+  @NotNull(message = "{valid.common.required}")
+  private Integer loginFailCnt;
+
+  @NotNull(message = "{valid.common.required}")
+  private Boolean accountLocked;
+
+  @NotEmpty(message = "{valid.common.required}")
+  private Set<String> roles;
 }
